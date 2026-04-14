a0e8ad8d14ef77645b01f50461e417148a083e7a
31614849 <3335283175079384-31614849@noreply.coze.cn>
2026-04-14 14:18:25 +0800

    fix: 优化选择磁贴动作配置项样式

--- src/components/ad-template-config.tsx
+++ src/components/ad-template-config.tsx
@@ -586,10 +586,10 @@ export function AdTemplateConfigPanel({
 
           {isBasicOpen && (
             <div className="px-4 pb-4 space-y-4">
-              {/* Action */}
+              {/* 动作 */}
               <div className="space-y-2">
                 <label className="text-xs text-gray-500">
-                  动作 <span className="text-red-500">*</span>
+                  动作
                 </label>
                 <Select
                   value={config.action}
@@ -601,18 +601,8 @@ export function AdTemplateConfigPanel({
                     <SelectValue />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectItem value="open">
-                      <div className="flex items-center gap-2">
-                        <FileText className="w-4 h-4" />
-                        <span>打开</span>
-                      </div>
-                    </SelectItem>
-                    <SelectItem value="show_image">
-                      <div className="flex items-center gap-2">
-                        <ImageIcon className="w-4 h-4" />
-                        <span>显示图片</span>
-                      </div>
-                    </SelectItem>
+                    <SelectItem value="open">打开</SelectItem>
+                    <SelectItem value="show_image">显示图片</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
